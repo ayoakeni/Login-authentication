e1a7530e83853cf1bfaa089a440d098249b92e5e
ayo akeni <ayoakeni64@gmail.com>
2024-05-19 22:47:02 +0100

    update

--- main.js
+++ main.js
@@ -44,13 +44,16 @@ function redirectToHomeIfLoggedIn(user) {
 
 async function redirectToLoginIfNotLoggedIn(user) {
   const allowedPages = ['/login.html', '/signup.html']; 
-  if (!user && !allowedPages.includes(window.location.pathname)) {
-    if (!sessionStorage.getItem('redirecting')) {
-      sessionStorage.setItem('redirecting', 'true');
-      window.location.href = 'login.html';
-    }
-  } else {
-    sessionStorage.removeItem('redirecting');
+  // if (!user && !allowedPages.includes(window.location.pathname)) {
+  //   if (!sessionStorage.getItem('redirecting')) {
+  //     sessionStorage.setItem('redirecting', 'true');
+  //     window.location.href = 'login.html';
+  //   }
+  // } else {
+  //   sessionStorage.removeItem('redirecting');
+  // }
+  if (!user && window.location.pathname === '/index.html') {
+    window.location.href = 'login.html';
   }
   if (user && window.location.pathname === '/signup.html') {
     await signOut(auth);
